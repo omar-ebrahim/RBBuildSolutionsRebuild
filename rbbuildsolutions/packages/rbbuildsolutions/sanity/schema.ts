9b6d2c2f5bd48890517bb22f8eed57f1a24caa6b
Omar Ebrahim <omar.ebrahim.reactdeveloper@gmail.com>
2024-07-07 13:12:06 +0100

    Removed buildtype from project

--- rbbuildsolutions/packages/rbbuildsolutions/sanity/schema.ts
+++ rbbuildsolutions/packages/rbbuildsolutions/sanity/schema.ts
@@ -1,6 +1,5 @@
 import { type SchemaTypeDefinition } from 'sanity'
-import { BuildType } from "@rbbuildsolutions/data/schemas/buildType"
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [BuildType],
+  types: [],
 }
